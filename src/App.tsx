71798b9e62199cfa36d51fe39d3f969e631a3297
wimbarelds <wim.barelds@gmail.com>
2025-08-06 20:25:54 +0200

    Better responsive scaling

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Content } from './Content';
 
 export function App() {
   return (
-    <main className="z-10 max-w-3xl p-12 w-full mx-auto bg-gray-950/50">
+    <main className="bg-gray-950/50 w-full  p-4 lg:p-12 lg:max-w-3xl lg:mx-auto">
       <MDXProvider components={{ wrapper: Prose }}>
         <Content />
       </MDXProvider>
